eea4760db72259afa168c5fe2ad77eb5d4cc56eb
Aldair9819 <150365649+Aldair9819@users.noreply.github.com>
2023-12-06 17:09:47 +0000

    checandoJalaCode

--- Proyecto4/LecturaDatos/LectorTXT.java
+++ Proyecto4/LecturaDatos/LectorTXT.java
@@ -16,7 +16,7 @@ public class LectorTXT {
 	//private final String NombreArchivo = "codigo2.txt";
 	//private final String NombreCarpeta = "codigos";
 	//private final String Ruta = NombreCarpeta+ "\\"+NombreArchivo;
-	private final String Ruta = "*Proyecto4/LecturaDatos/codigos/codigo3.txt";
+	private final String Ruta = "Proyecto4/LecturaDatos/codigos/codigo3.txt";
 	
 	// nombreFuncion, comandos (Stack), parametros, tipo de retorno
 	private HashMap<String, Funcion > funciones = new HashMap<>();
@@ -44,6 +44,7 @@ public class LectorTXT {
 	}
 	
 	public void iniciar() {
+		System.out.println(Ruta);
 		organizarPrograma(leerTexto());
 		getDatosTotales();
 	}
